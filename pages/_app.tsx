027514a32fe26934052f2b0253eb4929071be7bb
raymon <neocode.gs@gmail.com>
2023-10-28 15:44:12 +0800

    add idtoken

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,14 @@
-import type { AppProps } from 'next/app'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-import { SessionProvider } from 'next-auth/react'
-import { getLayout } from '@vercel/examples-ui'
-
-import '@vercel/examples-ui/globals.css'
+import "@vercel/examples-ui/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
-
   return (
-    <Layout
-      title="Securing routes with next-auth"
-      path="solutions/subdomain-auth"
-      description="How to secure subdomains using next-auth"
-    >
-      <SessionProvider>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </Layout>
-  )
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default App
+export default App;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
 
 const useSecureCookies = !!process.env.VERCEL_URL
 
@@ -8,8 +10,15 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      checks: 'pkce',
     }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: 'pkce'
+    })
   ],
+  debug: true,
   secret: process.env.SECRET as string,
   cookies: {
     sessionToken: {
@@ -18,7 +27,7 @@ export default NextAuth({
         httpOnly: true,
         sameSite: 'lax',
         path: '/',
-        domain: '.solutions-subdomain-auth.vercel.sh',
+        // domain: '.vercel.app',
         secure: useSecureCookies,
       },
     },
